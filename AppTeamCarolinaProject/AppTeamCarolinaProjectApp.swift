766dd46d07f9dff19ebae0830db356a49162f8cb
threadous <108011870+threadous@users.noreply.github.com>
2025-08-30 22:11:06 -0400

    Initial Commit

---
+++ AppTeamCarolinaProject/AppTeamCarolinaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTeamCarolinaProjectApp.swift
+//  AppTeamCarolinaProject
+//
+//  Created by Abhiram Kancharla on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AppTeamCarolinaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
